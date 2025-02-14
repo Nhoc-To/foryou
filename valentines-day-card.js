78a79a82de0729509683776ec944dac65c20b552
Nhoc-To <daokhanhduyhy2005@gmail.com>
2025-02-14 17:06:37 +0700

    d

--- valentines-day-card.js
+++ valentines-day-card.js
@@ -1,10 +1,17 @@
-$('.js-open-envelope').on('click', function (event) {
-  event.preventDefault();
-  var $self = $(this);
-  $self.find('.envelope').removeClass('tossing').addClass('open');
-  $self.find('.heart use').attr("xlink:href", "#icon-heart-broken");
-  $self.find('.envelope__card').addClass('open');
-});
-$('.js-surprise-button').on('click', function () {
-  window.location.href = "1/1.html"; // Thay bằng link của bạn
+$(document).ready(function () {
+  $('.js-open-envelope').on('click', function (event) {
+    event.preventDefault();
+    var $self = $(this);
+    $self.find('.envelope').removeClass('tossing').addClass('open');
+    $self.find('.heart use').attr("xlink:href", "#icon-heart-broken");
+    $self.find('.envelope__card').addClass('open');
+  });
+
+  $('.js-surprise-button').on('click', function (event) {
+    event.preventDefault(); // Ngăn chặn chuyển trang ngay lập tức
+    $('body').css('opacity', '0'); // Làm mờ dần trang
+    setTimeout(() => {
+      window.location.href = "1/1.html"; // Thay bằng link của bạn
+    }, 500); // Chờ 500ms để hiệu ứng hoàn thành
+  });
 });
